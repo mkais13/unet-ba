1a1cd6266aa0af55966b8210f0aebc31299fec4e
mkais13 <m_kais13@uni-muenster.de>
2022-01-19 15:16:02 +0100

    added first argparse args

--- main.py
+++ main.py
@@ -1,8 +1,14 @@
 from model import *
 from data import *
+import argparse
 
 #os.environ["CUDA_VISIBLE_DEVICES"] = "0"
-#testcomment
+
+
+parser = argparse.ArgumentParser(description='set Hyperparameters for training')
+parser.add_argument('--e' , '--epochs', type=int, metavar='', required=True, help='Number of Epochs')
+parser.add_argument('--s' , '--steps', type=int, metavar='', required=True, help='Number of Steps per Epoch')
+args = parser.parse_args()
 
 data_gen_args = dict(rotation_range=0.2,
                     width_shift_range=0.05,
@@ -15,7 +21,7 @@ myGene = trainGenerator(2,'data/membrane/train','image','label',data_gen_args,sa
 
 model = unet()
 model_checkpoint = ModelCheckpoint('unet_membrane.hdf5', monitor='loss',verbose=1, save_best_only=True)
-model.fit_generator(myGene,steps_per_epoch=150,epochs=2,callbacks=[model_checkpoint])
+model.fit_generator(myGene,steps_per_epoch=args.steps,epochs=args.epochs,callbacks=[model_checkpoint])
 
 testGene = testGenerator("data/membrane/test")
 results = model.predict_generator(testGene,30,verbose=1)
