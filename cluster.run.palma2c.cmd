1f5cf3ff400d43bef4904e0265990486db3173ce
mkais13 <m_kais13@uni-muenster.de>
2022-01-24 16:35:53 +0100

    fast push

--- cluster.run.palma2c.cmd
+++ cluster.run.palma2c.cmd
@@ -26,7 +26,7 @@
 module load fosscuda/2019a
 ml Singularity
 cd $HOME/unet
-singularity exec tensorflow_1.10.1-devel-gpu-py3.sif python {python_script}
+singularity exec --nv tensorflow_1.10.1-devel-gpu-py3.sif python {python_script}
 
 
 
