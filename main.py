095ca63e84bcb27750561e9c260fe1d733f5e5c4
mkais13 <m_kais13@uni-muenster.de>
2022-02-06 16:23:59 +0100

    fast push

--- main.py
+++ main.py
@@ -6,8 +6,10 @@ import argparse
 #testcomment 
 
 parser = argparse.ArgumentParser(description='set Hyperparameters for training')
-parser.add_argument('--e' , '--epochs', type=int, metavar='', required=True, help='Number of Epochs')
-parser.add_argument('--s' , '--steps', type=int, metavar='', required=True, help='Number of Steps per Epoch')
+parser.add_argument('--e' , '--epochs', type=int, metavar='', nargs='?', default=5, const=5, help='Number of Epochs')
+parser.add_argument('--s' , '--steps', type=int, metavar='', nargs='?', default=300, const=300, help='Number of Steps per Epoch')
+parser.add_argument('--bs' , '--batchsize', type=int, metavar='',nargs='?', default=2, const=2, help='Batch Size')
+parser.add_argument('--lf' , '--lossfunction', metavar='',nargs='?', default='binary_crossentropy', const='binary_crossentropy', help='Loss Function for the Model')
 args = parser.parse_args()
 
 data_gen_args = dict(rotation_range=0.2,
@@ -17,12 +19,15 @@ data_gen_args = dict(rotation_range=0.2,
                     zoom_range=0.05,
                     horizontal_flip=True,
                     fill_mode='nearest')
-myGene = trainGenerator(2,'data/membrane/train','image','label',data_gen_args,save_to_dir = None)
 
-model = unet()
-model_checkpoint = ModelCheckpoint('unet_membrane.hdf5', monitor='loss',verbose=1, save_best_only=True)
-model.fit_generator(myGene,steps_per_epoch=args.steps,epochs=args.epochs,callbacks=[model_checkpoint])
+
+myGene = trainGenerator(args.batchsize,'data/membrane/train','image','label',data_gen_args,save_to_dir = None)
+
+model = unet(args.lossfunction)
+#model_checkpoint = ModelCheckpoint('unet_membrane.hdf5', monitor='loss',verbose=1, save_best_only=True)
+#model.fit_generator(myGene,steps_per_epoch=args.steps,epochs=args.epochs,callbacks=[model_checkpoint])
+model.fit_generator(myGene,steps_per_epoch=args.steps,epochs=args.epochs)
 
 testGene = testGenerator("data/membrane/test")
 results = model.predict_generator(testGene,30,verbose=1)
-saveResult("data/membrane/test",results)
+#saveResult("data/membrane/test",results)
--- model.py
+++ model.py
@@ -10,7 +10,7 @@ from keras.callbacks import ModelCheckpoint, LearningRateScheduler
 from keras import backend as keras
 
 
-def unet(pretrained_weights = None,input_size = (256,256,1)):
+def unet(loss_function, pretrained_weights = None,input_size = (256,256,1)):
     inputs = Input(input_size)
     conv1 = Conv2D(64, 3, activation = 'relu', padding = 'same', kernel_initializer = 'he_normal')(inputs)
     conv1 = Conv2D(64, 3, activation = 'relu', padding = 'same', kernel_initializer = 'he_normal')(conv1)
@@ -54,7 +54,7 @@ def unet(pretrained_weights = None,input_size = (256,256,1)):
 
     model = Model(input = inputs, output = conv10)
 
-    model.compile(optimizer = Adam(lr = 1e-4), loss = 'binary_crossentropy', metrics = ['accuracy'])
+    model.compile(optimizer = Adam(lr = 1e-4), loss = loss_function, metrics = ['accuracy'])
     
     #model.summary()
 
