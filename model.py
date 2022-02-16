d690f30d64e484a6b7865cb78ba0adb37f915936
mkais13 <m_kais13@uni-muenster.de>
2022-02-16 15:22:28 +0100

    fast push

--- model.py
+++ model.py
@@ -13,13 +13,13 @@ from keras import backend as keras
 
 
 
-def DiceLoss(y_true, y_pred, smooth=None):
+def DiceLoss(y_true, y_pred, smooth=0):
     y_true_f = K.flatten(y_true)
     y_pred_f = K.flatten(y_pred)
     intersection = K.sum(y_true_f * y_pred_f)
     return 1 - (2. * intersection + smooth) / (K.sum(y_true_f) + K.sum(y_pred_f) + smooth)
 
-def DiceBCELoss(y_true, y_pred, smooth=None):
+def DiceBCELoss(y_true, y_pred, smooth=0):
     y_true_f = K.flatten(y_true)
     y_pred_f = K.flatten(y_pred)
     BCE =  K.binary_crossentropy(y_true_f, y_pred_f)
