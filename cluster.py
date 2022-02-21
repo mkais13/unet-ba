ba66642eef9e6b4381ec608851feceadfe195fa5
mkais13 <m_kais13@uni-muenster.de>
2022-02-21 19:09:25 +0100

    fast push

--- cluster.py
+++ cluster.py
@@ -52,8 +52,18 @@ optimizers = [
     "Adam"
 ]
 
+topologyfactors = [
+    0.5, 
+    1, 
+    2
+]
 
-batchsizes = [3, 10, 30]
+
+batchsizes = [
+    3, 
+    10, 
+    30
+]
 
 lossfunctions = [
     "mean_squared_error",
@@ -61,16 +71,19 @@ lossfunctions = [
     "focal_tversky"
 ]
 
-kernelinitializer = [
+kernelinitializers = [
     "he_normal",
-    "he_uniform",
+    "he_uniform"
 ]
 
-#for opt in optimizers:
-#        for bs in batchsizes:
-#            for lf in lossfunctions:
-#                commands.append("main.py -e 10 -bs {0} -lf {1} -opt {2}".format(bs,lf,opt))
-commands.append("main.py -e 5 -bs {0} -lf {1} -opt {2} -tf {3} -ki {4}".format(3,"mean_squared_error","Adagrad",2,"he_uniform"))
+
+#for bs in batchsizes:
+ #   for lf in lossfunctions:
+#        for opt in optimizers:
+#            for tf in topologyfactors:
+#                for ki in kernelinitializers:
+#                    commands.append("main.py -e 5 -bs {0} -lf {1} -opt {2} -tf {3} -ki{4} ".format(bs,lf,opt,tf,ki))
+commands.append("main.py -e 5 -bs {0} -lf {1} -opt {2} -tf {3} -ki{4} ".format(2,"mean_squared_error","SGD",0.5,"he_uniform"))
 
 
 ssh = get_ssh_connection(HOSTS[0])
--- main.py
+++ main.py
@@ -33,7 +33,7 @@ model = unet(args.lossfunction, args.optimizer, args.topologyfactor, args.kernel
 dirpath = '/scratch/tmp/m_kais13/checkpoints'
 os.makedirs(dirpath, exist_ok=True)
 filename = 'bs{0}-lf{1}-opt{2}-tf{3}-ki{4}.h5'.format(args.batchsize,args.lossfunction,args.optimizer, args.topologyfactor, args.kernelinitializer)
-cb_checkpointer = ModelCheckpoint(filepath = os.path.join(dirpath,"e{epoch}"+filename), monitor = 'loss', mode = 'auto', verbose=1)
+cb_checkpointer = ModelCheckpoint(filepath = os.path.join(dirpath,filename+"-e{epoch}"), monitor = 'loss', mode = 'auto', verbose=1)
 #model_checkpoint = ModelCheckpoint("/scratch/tmp/m_kais13/checkpoints/unetmembranetest.h5", monitor='loss',verbose=1, save_best_only=False)
 num_images = 30
 model.fit_generator(myGene,steps_per_epoch=(num_images/args.batchsize),epochs=args.epochs,callbacks=[cb_checkpointer])
