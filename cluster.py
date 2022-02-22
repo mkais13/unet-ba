0e068426fcce623ceaab8eb1bac346e1480ed673
mkais13 <m_kais13@uni-muenster.de>
2022-02-22 09:59:53 +0100

    fast push

--- cluster.py
+++ cluster.py
@@ -77,13 +77,13 @@ kernelinitializers = [
 ]
 
 
-#for bs in batchsizes:
- #   for lf in lossfunctions:
-#        for opt in optimizers:
-#            for tf in topologyfactors:
-#                for ki in kernelinitializers:
-#                    commands.append("main.py -e 5 -bs {0} -lf {1} -opt {2} -tf {3} -ki {4} ".format(bs,lf,opt,tf,ki))
-commands.append("main.py -e 5 -bs {0} -lf {1} -opt {2} -tf {3} -ki {4} ".format(2,"mean_squared_error","SGD",0.5,"he_uniform"))
+for bs in batchsizes:
+    for lf in lossfunctions:
+        for opt in optimizers:
+            for tf in topologyfactors:
+                for ki in kernelinitializers:
+                    commands.append("main.py -e 5 -bs {0} -lf {1} -opt {2} -tf {3} -ki {4} ".format(bs,lf,opt,tf,ki))
+#commands.append("main.py -e 5 -bs {0} -lf {1} -opt {2} -tf {3} -ki {4} ".format(2,"mean_squared_error","SGD",0.5,"he_uniform"))
 
 
 ssh = get_ssh_connection(HOSTS[0])
