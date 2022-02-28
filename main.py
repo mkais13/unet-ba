3347ab562596334a7c0ad97e9ab80a85a629de40
mkais13 <m_kais13@uni-muenster.de>
2022-02-28 20:54:44 +0100

    fast push

--- main.py
+++ main.py
@@ -30,14 +30,14 @@ data_gen_args = dict(rotation_range=0.2,
 myGene = trainGenerator(args.batchsize,'data/membrane/train','image','label',data_gen_args,save_to_dir = None)
 run_identifier = 'bs{0}-lf{1}-opt{2}-tf{3}-ki{4}'.format(args.batchsize,args.lossfunction,args.optimizer, args.topologyfactor, args.kernelinitializer)
 model = unet(args.lossfunction, args.optimizer, args.topologyfactor, args.kernelinitializer)
-checkpointpath = '/scratch/tmp/m_kais13/checkpoints'
+checkpointpath = '/scratch/tmp/m_kais13/checkpoints/' + run_identifier
 tensorboardpath = '/scratch/tmp/m_kais13/losslogs'
 resultpath = '/scratch/tmp/m_kais13/results/' + run_identifier
 os.makedirs(tensorboardpath, exist_ok=True)
 os.makedirs(checkpointpath, exist_ok=True)
 os.makedirs(resultpath, exist_ok=True)
 cb_tensorboard = TensorBoard(log_dir= os.path.join(tensorboardpath, run_identifier))
-cb_checkpointer = ModelCheckpoint(filepath = os.path.join(checkpointpath,run_identifier+"-e{epoch}.h5"), monitor = 'loss', mode = 'auto', verbose=1)
+cb_checkpointer = ModelCheckpoint(filepath = checkpointpath +"/-e{epoch}.h5", monitor = 'loss', mode = 'auto', verbose=1)
 #model_checkpoint = ModelCheckpoint("/scratch/tmp/m_kais13/checkpoints/unetmembranetest.h5", monitor='loss',verbose=1, save_best_only=False)
 num_images = 30
 model.fit_generator(myGene,steps_per_epoch=(num_images/args.batchsize),epochs=args.epochs,callbacks=[cb_checkpointer, cb_tensorboard])
