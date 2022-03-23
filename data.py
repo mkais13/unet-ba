524e72b4b6219c84b34664c5dddc81aa7f2b20e7
mkais13 <m_kais13@uni-muenster.de>
2022-03-23 14:53:03 +0100

    fast push

--- data.py
+++ data.py
@@ -122,3 +122,37 @@ def saveResult(save_path,npyfile,flag_multi_class = False,num_class = 2):
     for i,item in enumerate(npyfile):
         img = labelVisualize(num_class,COLOR_DICT,item) if flag_multi_class else item[:,:,0]
         io.imsave(os.path.join(save_path,"%d_predict.png"%i),img)
+
+
+def validGenerator(batch_size, val_path, image_folder, mask_folder, image_color_mode="grayscale", mask_color_mode="grayscale", image_save_prefix="val_image", mask_save_prefix="val_mask", flag_multi_class=False, num_class=2, save_to_dir=None,
+target_size=(256,256), seed=1):
+    image_datagen = ImageDataGenerator()
+    mask_datagen = ImageDataGenerator()
+
+    image_generator = image_datagen.flow_from_directory(
+    val_path,
+    classes = [image_folder],
+    class_mode = None,
+    color_mode = image_color_mode,
+    target_size = target_size,
+    batch_size = batch_size,
+    save_to_dir = save_to_dir,
+    save_prefix  = image_save_prefix,
+    seed = seed)
+
+    mask_generator = mask_datagen.flow_from_directory(
+    val_path,
+    classes = [mask_folder],
+    class_mode = None,
+    color_mode = mask_color_mode,
+    target_size = target_size,
+    batch_size = batch_size,
+    save_to_dir = save_to_dir,
+    save_prefix  = mask_save_prefix,
+    seed = seed)
+
+    train_generator = zip(image_generator, mask_generator)
+
+    for (img, mask) in train_generator:
+        img, mask = adjustData(img,mask, flag_multi_class, num_class)
+        yield (img, mask)
--- main.py
+++ main.py
@@ -7,10 +7,10 @@ import argparse
 #testcomment 
 
 parser = argparse.ArgumentParser(description='set Hyperparameters for training')
-parser.add_argument('-e' , '--epochs', type=int, metavar='epochs', nargs='?', default=1, const=1, help='Number of Epochs')
+parser.add_argument('-e' , '--epochs', type=int, metavar='epochs', nargs='?', default=3, const=1, help='Number of Epochs')
 #parser.add_argument('-s' , '--steps', type=int, metavar='steps', nargs='?', default=300, const=300, help='Number of Steps per Epoch')
 parser.add_argument('-bs' , '--batchsize', type=int, metavar='batchsize',nargs='?', default=3, const=3, help='Batch Size')
-parser.add_argument('-lf' , '--lossfunction', metavar='lossfunction',nargs='?', default='msssim', const='msssim', help='loss function for the Model')
+parser.add_argument('-lf' , '--lossfunction', metavar='lossfunction',nargs='?', default='binary_crossentropy', const='binary_crossentrop', help='loss function for the Model')
 parser.add_argument('-ki' , '--kernelinitializer', metavar='kernelinitializer',nargs='?', default='he_normal', const='he_normal', help='kernel initializer for the Model')
 parser.add_argument('-opt' , '--optimizer', metavar='optimizer',nargs='?', default="Adam", const="Adam", help='optimizer function for the model')
 #parser.add_argument('-lr' , '--learningrate' , type=float, metavar='learningrate',nargs='?', default= 1e-4, const= 1e-4, help='learning rate for the model')
@@ -25,22 +25,61 @@ data_gen_args = dict(rotation_range=0.2,
                     horizontal_flip=True,
                     fill_mode='nearest')
                 
-
+num_images = 30
 
 myGene = trainGenerator(args.batchsize,'data/membrane/train','image','label',data_gen_args,save_to_dir = None)
 run_identifier = 'bs{0}-lf{1}-opt{2}-tf{3}-ki{4}'.format(args.batchsize,args.lossfunction,args.optimizer, args.topologyfactor, args.kernelinitializer)
 model = unet(args.lossfunction, args.optimizer, args.topologyfactor, args.kernelinitializer)
-checkpointpath = '/scratch/tmp/m_kais13/checkpoints/' + run_identifier
-tensorboardpath = '/scratch/tmp/m_kais13/losslogs'
-resultpath = '/scratch/tmp/m_kais13/results2/' + run_identifier
+checkpointpath = '/scratch/tmp/m_kais13/run3/checkpoints/' + run_identifier
+tensorboardpath = '/scratch/tmp/m_kais13/run3/losslogs'
+resultpath = '/scratch/tmp/m_kais13/run3/results/' + run_identifier
 os.makedirs(tensorboardpath, exist_ok=True)
 os.makedirs(checkpointpath, exist_ok=True)
 os.makedirs(resultpath, exist_ok=True)
-cb_tensorboard = TensorBoard(log_dir= os.path.join(tensorboardpath, run_identifier))
+
+
+validGene = validGenerator(args.batchsize, "data/membrane/train", "image", "label")
+
+class TensorBoardWrapper(TensorBoard):
+    '''Sets the self.validation_data property for use with TensorBoard callback.'''
+
+    def __init__(self, batch_gen, nb_steps, b_size, **kwargs):
+        super(TensorBoardWrapper, self).__init__(**kwargs)
+        self.batch_gen = batch_gen # The generator.
+        self.nb_steps = nb_steps   # Number of times to call next() on the generator.
+        #self.batch_size = b_size
+
+    def on_epoch_end(self, epoch, logs):
+        # Fill in the `validation_data` property. Obviously this is specific to how your generator works.
+        # Below is an example that yields images and classification tags.
+        # After it's filled in, the regular on_epoch_end method has access to the validation_data.
+        imgs, tags = None, None
+        for s in range(self.nb_steps):
+            ib, tb = next(self.batch_gen)
+            if imgs is None and tags is None:
+                imgs = np.zeros(((self.nb_steps * self.batch_size,) + ib.shape[1:]), dtype=np.float32)
+                tags = np.zeros(((self.nb_steps * self.batch_size,) + tb.shape[1:]), dtype=np.uint8)
+            imgs[s * ib.shape[0]:(s + 1) * ib.shape[0]] = ib
+            tags[s * tb.shape[0]:(s + 1) * tb.shape[0]] = tb
+        
+        self.validation_data = [imgs, tags, np.ones(imgs.shape[0]), 0.0]
+              
+        return super(TensorBoardWrapper, self).on_epoch_end(epoch, logs)
+
+
+
+
+
+
+
+
+cb_tensorboard = TensorBoardWrapper(validGene,int(num_images/args.batchsize), args.batchsize, log_dir= os.path.join(tensorboardpath, run_identifier), histogram_freq=1, batch_size=args.batchsize)
+
+#cb_tensorboard = TensorBoard(log_dir= os.path.join(tensorboardpath, run_identifier), histogram_freq=1,write_grads=True, write_graph=True)
 cb_checkpointer = ModelCheckpoint(filepath = os.path.join(checkpointpath,run_identifier+"-e{epoch}.h5"), monitor = 'loss', mode = 'auto', verbose=1)
 #model_checkpoint = ModelCheckpoint("/scratch/tmp/m_kais13/checkpoints/unetmembranetest.h5", monitor='loss',verbose=1, save_best_only=False)
-num_images = 30
-model.fit_generator(myGene,(num_images/args.batchsize)*10,epochs=args.epochs,callbacks=[cb_checkpointer, cb_tensorboard])
+
+model.fit_generator(myGene,(num_images/args.batchsize)*10,epochs=args.epochs,callbacks=[cb_checkpointer, cb_tensorboard] , validation_data=validGene, validation_steps=3)
 #model.fit_generator(myGene,steps_per_epoch=args.steps,epochs=args.epochs)
 #model.save("/scratch/tmp/m_kais13/checkpoints/unetmembranetest")
 
